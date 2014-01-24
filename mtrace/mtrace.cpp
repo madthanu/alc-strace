1b953950ffd641eb138e5c47dec7ca8a4049f04a
madthanu <madthanu@madlap.madnet>
2014-01-24 03:33:56 -0600

    tmp

--- mtrace/mtrace.cpp
+++ mtrace/mtrace.cpp
@@ -1,17 +1,134 @@
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#include <syscall.h>
 #include <time.h>
 #include <unistd.h>
 #include "pin.H"
 
 KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "-", "trace file");
 
+struct syscall_details {
+	ADDRINT ip;
+	ADDRINT num;
+	ADDRINT args[6];
+};
+
 LOCALVAR VOID *WriteEa[PIN_MAX_THREADS];
 LOCALVAR FILE *out_file[PIN_MAX_THREADS];
+LOCALVAR struct syscall_details thread_to_syscall[PIN_MAX_THREADS];
+PIN_LOCK globalLock;
 bool standard_out;
+
+class MemregionTracker {
+
+	#define MAX_MEM_REGIONS 2000
+	struct t_region {
+		void* addr_start;
+		void* addr_end;
+	};
+
+	private:
+	static struct t_region region[MAX_MEM_REGIONS];
+	static int region_last; // initialized to 0
+
+	static void _insert(void *addr_start, void *addr_end) {
+		int i;
+		for(i = 0; i < region_last; i++) {
+			if(region[i].addr_start == 0) {
+				region[i].addr_start = addr_start;
+				region[i].addr_end = addr_end;
+				break;
+			}
+		}
+
+		if(i == region_last) {
+			assert(region_last < MAX_MEM_REGIONS);
+			region[region_last].addr_start = addr_start;
+			region[region_last].addr_end = addr_end;
+			region_last++;
+		}
+	}
+
+	static void _remove(int i) {
+		if(i == region_last - 1) {
+			region_last--;
+		} else {
+			region[i].addr_start = 0;
+		}
+	}
+
+	static int _find_overlap(void *addr_start, void *addr_end) {
+		for(int i = 0; i < region_last; i++) {
+			if(region[i].addr_start != 0) {
+				void *cur_start = region[i].addr_start;
+				void *cur_end = region[i].addr_end;
+				if ((addr_start >= cur_start && addr_start <= cur_end) ||
+					(addr_end >= cur_start && addr_end <= cur_end) ||
+					(cur_start >= addr_start && cur_start <= addr_end) ||
+					(cur_end >= addr_start && cur_end <= addr_end))
+				{
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+
+	static void _remove_overlaps(void *addr_start, void *addr_end, bool entire_regions) {
+		while(true) {
+			int i = _find_overlap(addr_start, addr_end);
+			if(i == -1) {
+				return;
+			}
+			void *found_start = region[i].addr_start;
+			void *found_end = region[i].addr_end;
+			_remove(i);
+			if(!entire_regions) {
+				if (found_start < addr_start) {
+					_insert(found_start, ((UINT8 *)addr_start) - 1);
+				}
+				if (found_end > addr_end) {
+					_insert(((UINT8 *)addr_end) + 1, found_end);
+				}
+			}
+		}
+	}
+
+	public:
+	static void insert(void *addr_start, void *addr_end) {
+		PIN_GetLock(&globalLock, 1);
+		assert(_find_overlap(addr_start, addr_end) == -1);
+		_insert(addr_start, addr_end);
+		PIN_ReleaseLock(&globalLock);
+	}
+
+	static void remove_overlaps(void *addr_start, void *addr_end) {
+		PIN_GetLock(&globalLock, 1);
+		_remove_overlaps(addr_start, addr_end, false);
+		PIN_ReleaseLock(&globalLock);
+	}
+
+	static void remove_overlapping_regions(void *addr_start, void *addr_end) {
+		PIN_GetLock(&globalLock, 1);
+		_remove_overlaps(addr_start, addr_end, true);
+		PIN_ReleaseLock(&globalLock);
+	}
+
+	static bool address_mapped(void *addr) {
+		int i;
+		PIN_GetLock(&globalLock, 1);
+		i = _find_overlap(addr, addr);
+		PIN_ReleaseLock(&globalLock);
+		return (i != -1);
+	}
+};
+
+struct MemregionTracker::t_region MemregionTracker::region[MAX_MEM_REGIONS];
+int MemregionTracker::region_last;
 
 void mprintf(const char *format, ...)
 {
@@ -56,10 +173,12 @@ VOID EmitWrite(THREADID threadid, UINT32 size) {
 	char bytes[101];
 	VOID *ea = WriteEa[threadid];
 
-	PIN_SafeCopy(&bytes[0], static_cast<char *>(ea), size);
-	bytes[size + 1] = '\0';
-	PrintTime();
-	mprintf("W %p (%u bytes): '%s'\n", ea, size, bytes);
+	if(MemregionTracker::address_mapped(ea)) {
+		PIN_SafeCopy(&bytes[0], static_cast<char *>(ea), size);
+		bytes[size + 1] = '\0';
+		PrintTime();
+		mprintf("	W %p (%u bytes): '%s'\n", ea, size, bytes);
+	}
 }
 
 VOID Instruction(INS ins, VOID * v) {
@@ -112,32 +231,123 @@ INT32 Usage() {
 	return -1;
 }
 
+VOID Image(IMG img, VOID * v) {
+	static int already_executed = 0;
+	if(!already_executed) {
+		already_executed = 1;
+
+		char temp[10];
+		sprintf(temp, "%u", getpid());
+
+		pid_t pid = fork();
+		if(!pid) {
+			execlp("strace", "strace", "-ff", "-tt", "-o", "tmp/trace", "-p", temp, NULL);
+			assert(false);
+		}
+	}
+}
+
+VOID SyscallEntry(THREADID threadIndex, CONTEXT *ctxt, SYSCALL_STANDARD std, VOID *v)
+{
+	ADDRINT num = PIN_GetSyscallNumber(ctxt, std);
+
+	if (num != SYS_mmap && num != SYS_munmap) {
+		thread_to_syscall[threadIndex].ip = 0;
+		return;
+	}
+
+	thread_to_syscall[threadIndex].ip = PIN_GetContextReg(ctxt, REG_INST_PTR);
+	thread_to_syscall[threadIndex].num = num;
+
+	if(num == SYS_mmap) {
+		for(int i = 0; i < 6; i++) {
+			#if defined(TARGET_LINUX) && defined(TARGET_IA32)
+			thread_to_syscall[threadIndex].args[i] =
+				(reinterpret_cast<ADDRINT *>(PIN_GetSyscallArgument(ctxt, std, 0)))[i];
+			#else
+			thread_to_syscall[threadIndex].args[i] = PIN_GetSyscallArgument(ctxt, std, i);
+			#endif
+		}
+	} else {
+		for(int i = 0; i < 2; i++) {
+			thread_to_syscall[threadIndex].args[i] = PIN_GetSyscallArgument(ctxt, std, i);
+		}
+	}
+}
+
+VOID SyscallExit(THREADID threadIndex, CONTEXT *ctxt, SYSCALL_STANDARD std, VOID *v) {
+	if(thread_to_syscall[threadIndex].ip == 0)
+		return;
+
+	if(thread_to_syscall[threadIndex].num == SYS_mmap) {
+		void *ret = (void *)(PIN_GetSyscallReturn(ctxt, std));
+
+		assert(ret != NULL);
+		if(ret != MAP_FAILED) {
+			ADDRINT *args = &thread_to_syscall[threadIndex].args[0];
+			void *given_addr = (void *)(*args++);
+			size_t length = (size_t)(*args++);
+			int prot = (int)(*args++);
+			int flags = (int)(*args++);
+			int fd = (int)(*args++);
+			off_t offset = (off_t)(*args++);
+
+			printf("mmap(%p, %lu, %d, %d, %d, %lu) = %p\n", given_addr, length, prot, flags, fd, offset, ret);
+
+			void *addr_start = ret;
+			void *addr_end = (void *)((UINT8 *) addr_start + length - 1);
+			(void) offset;
+			if(flags & MAP_FIXED) {
+				MemregionTracker::remove_overlaps(addr_start, addr_end);
+			}
+
+			if((flags & MAP_SHARED) &&
+				(prot & PROT_WRITE) &&
+				!(flags & MAP_ANON) &&
+				!(flags & MAP_ANONYMOUS)
+				&& (fd > 0))
+			{
+				MemregionTracker::insert(addr_start, addr_end);
+			}
+
+		}
+	}
+
+	if(thread_to_syscall[threadIndex].num == SYS_munmap) {
+		int ret = static_cast<int>(PIN_GetSyscallReturn(ctxt, std));
+
+		if(ret != -1) {
+			ADDRINT *args = &thread_to_syscall[threadIndex].args[0];
+			void *addr_start = (void *)(*args++);
+			size_t length = (size_t)(*args++);
+
+			printf("munmap(%p, %lu) = %d\n", addr_start, length, ret);
+
+			void *addr_end = (void *)((UINT8 *) addr_start + length - 1);
+
+			MemregionTracker::remove_overlapping_regions(addr_start, addr_end);
+		}
+	}
+}
 
 int main(int argc, char *argv[]) {
 	PIN_InitSymbols();
  	if(PIN_Init(argc, argv))
 		return Usage();
+	PIN_InitLock(&globalLock);
 
-	bool strace = false;
-	for(int i = 0; i < argc; i++) {
-		if(!strcmp(argv[i], "--")) {
-			strace = !strcmp(argv[i + 1], "strace");
-			printf("Command: %s, strace = %d\n", argv[i + 1], strace);
-			break;
-		}
+	standard_out = false;
+	if(!strcmp(KnobOutputFile.Value().c_str(), "-")) {
+		standard_out = true;
+	} else {
+		memset(out_file, 0, sizeof(out_file));
 	}
 
-	if(!strace) {
-		standard_out = false;
-		if(!strcmp(KnobOutputFile.Value().c_str(), "-")) {
-			standard_out = true;
-		} else {
-			memset(out_file, 0, sizeof(out_file));
-		}
-
-		INS_AddInstrumentFunction(Instruction, 0);
-		PIN_AddFiniFunction(Fini, 0);
-	}
+	IMG_AddInstrumentFunction(Image, 0);
+	INS_AddInstrumentFunction(Instruction, 0);
+	PIN_AddSyscallEntryFunction(SyscallEntry, 0);
+	PIN_AddSyscallExitFunction(SyscallExit, 0);
+	PIN_AddFiniFunction(Fini, 0);
 
 	PIN_StartProgram();
 
--- mtrace/test.c
+++ mtrace/test.c
@@ -1,15 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <assert.h>
+#include <malloc.h>
+
+char *strings[10];
+
+//delim = 0.004 s
+
+void initialize_strings() {
+	int i, j;
+	for(i = 0; i < 10; i++) {
+		strings[i] = malloc(110);
+		for(j = 0; j < 99; j++) {
+			strings[i][j] = 'a' + i;
+		}
+		strings[i][99] = '\0';
+		strings[i][100] = '\0';
+	}
+}
 
 int main() {
-	char *memregion = (char *) malloc(sizeof(char) * 5);
-	printf("memregion address = %p, starting assignments\n", memregion);
-	//sleep(10);
-	memregion[0] = '0';
-	memregion[1] = '1';
-	memregion[2] = '2';
-	memregion[3] = '3';
-	memregion[4] = '4';
-	printf("assignments done.\n");
-	//while(1);
+	system("rm -f /tmp/mmap1");
+	system("rm -f /tmp/mmap2");
+	system("rm -f /tmp/mmap3");
+
+	initialize_strings();
+
+	int fd[3];
+	fd[0] = open("/tmp/mmap1", O_RDWR | O_CREAT, 00666);
+	fd[1] = open("/tmp/mmap2", O_RDWR | O_CREAT, 00666);
+	fd[2] = open("/tmp/mmap3", O_RDWR | O_CREAT, 00666);
+
+	char *memregion[3];
+	int ret;
+	int string_cnt = 0;
+	int i;
+	for(i = 0; i < 3; i++) {
+		assert(fd[i] > 0);
+		ret = ftruncate(fd[i], 40960);
+		assert(ret == 0);
+		memregion[i] = mmap(NULL, 40960, PROT_WRITE, MAP_SHARED, fd[i], 0);
+		printf("Tracked memregion[%d] = %p; %c\n", i, memregion[i], strings[string_cnt][0]);
+		strcpy(memregion[i], strings[string_cnt++]);
+	}
+
+	char *someplace;
+
+	someplace = mmap(NULL, 4096, PROT_WRITE, MAP_PRIVATE, fd[0], 0);
+	printf("Untracked memregion = %p; %c\n", someplace, strings[string_cnt][0]);
+	strcpy(someplace, strings[string_cnt++]);
+
+	someplace = mmap(memregion[0], 4096, PROT_WRITE, MAP_PRIVATE | MAP_FIXED, fd[1], 0);
+	printf("Untracked memregion[0] = %p; %c\n", someplace, strings[string_cnt][0]);
+	strcpy(someplace, strings[string_cnt++]);
+
+	someplace = memregion[0] + 4096;
+	printf("Tracked memregion[0] + 4096 = %p; %c\n", someplace, strings[string_cnt][0]);
+	strcpy(someplace, strings[string_cnt++]);
+
+	munmap(memregion[1], 4096);
+	someplace = mmap(memregion[1], 4096, PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+	printf("Untracked memregion[1] = %p; %c\n", someplace, strings[string_cnt][0]);
+	strcpy(someplace, strings[string_cnt++]);
+
+	printf("done.\n");
 }
