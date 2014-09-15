81320c0629b7c3bd2e0c300dda9ef166e8bc3620
madthanu <madthanu@madlap.madnet>
2014-09-15 01:43:12 -0500

    Initial cleanup

--- mtrace/mtrace_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm -rf /tmp/mtrace_test
-mkdir -p /tmp/mtrace_test
-mtrace -s 40 -o /tmp/mtrace_test/trace -- ./a.out #strace -ff -tt -o tmp/trace ./a.out
-echo "Please verify workload out manually"
